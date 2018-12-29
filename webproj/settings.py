d5b4612b26eff19e7853fe986501819d8db4df39
mulyach <mulyach@gmail.com>
2018-12-29 22:56:44 +0700

    Replicate example

--- webproj/settings.py
+++ webproj/settings.py
@@ -157,7 +157,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],  #"hosts": [(os.environ.get('REDIS_URL', '127.0.0.1'),6379)],        #('127.0.0.1',6379)
+            "hosts": [(os.environ.get('REDIS_URL', '127.0.0.1'),6379)],  #"hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],          #('127.0.0.1',6379)
         },
         #'ROUTING': 'webproj.routing.channel_routing',
     },
