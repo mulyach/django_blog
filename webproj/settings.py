00a1a4695e05c52707c84d7a955277e8f2e368be
mulyach <mulyach@gmail.com>
2018-12-29 22:34:46 +0700

    Replicate example

--- webproj/settings.py
+++ webproj/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -165,4 +166,4 @@ CHANNEL_LAYERS = {
 
 IS_CI = os.environ.get('IS_CI', False)
 if not IS_CI:
-webproj.settings(locals())
+django_heroku.settings(locals())
