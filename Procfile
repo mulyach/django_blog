79284870efa772650ce729974bcb16a5a7c1626a
mulyach <mulyach@gmail.com>
2018-12-29 15:45:41 +0700

    Add extended board channel 3

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: daphne webproj.asgi:channel_layer --port $PORT --bind 0.0.0.0 -v2
+web: daphne webproj.asgi:application --port $port --bind 0.0.0.0
 chatworker: python manage.py runworker --settings=webproj.settings -v2
--- webproj/asgi.py
+++ webproj/asgi.py
@@ -6,9 +6,7 @@ defined in the ASGI_APPLICATION setting.
 import os
 import django
 from channels.routing import get_default_application
-from channels.asgi import get_channel_layer
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webproj.settings")
 django.setup()
 application = get_default_application()
-channel_layer = get_channel_layer()
