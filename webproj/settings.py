a020b9595f589420957aafe5a5a86c849c706a0a
mulyach <mulyach@gmail.com>
2018-12-29 14:57:04 +0700

    Add extended board channel

--- webproj/settings.py
+++ webproj/settings.py
@@ -156,7 +156,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1',6379)],
+            "hosts": [('lkestories.herokuapp.com',6379)],       #127.0.0.1
         },
     },
 }
